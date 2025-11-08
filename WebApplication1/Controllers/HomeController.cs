64d7bb5ee2274434a37f84bf6a567f0c9e4ba8c5
Radiant213 <galangmaruff1@gmail.com>
2025-11-08 15:00:43 +0700

    Add files via upload

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    //home
+    [ApiController]
+    [Route("[controller]")]
+    public class apaController : Controller
+    {
+        [HttpGet("apa")]
+        public string Apa()
+        {
+            return "Apa kabar?";
+        }
+
+        [HttpGet("hai")]
+        public string Hai([FromQuery] string nama)
+        {
+            return "Hello, " + nama + "!";
+        }
+    }
+}
